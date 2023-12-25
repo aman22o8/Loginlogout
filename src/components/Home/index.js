636cdc86ebb134ce97950678e92c945eee07011a
aman2208 <amankumar.kumar369@gamail.com>
2023-12-25 18:31:30 +0000

    seconf commit 25/12/2023 00:00

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -18,6 +18,10 @@ class Home extends Component {
     return (
       <div className="main_container">
         <div className="card_container">
+          <h1>
+            {' '}
+            this my my second commit i made in the same file 25/12/2023 00:00
+          </h1>
           <Message display={isloggedIn} />
           <div>
             {isloggedIn ? (
